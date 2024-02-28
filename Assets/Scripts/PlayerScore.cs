5b35d61588dfaa1bfc8286c241c22dc8fcc30e3b
Aynur-AcerLaptop <aynurvarol369@gmail.com>
2024-02-28 15:55:23 +0300

    no message

--- Assets/Scripts/PlayerScore.cs
+++ Assets/Scripts/PlayerScore.cs
@@ -6,26 +6,37 @@ using UnityEngine.UI;
 public class PlayerScore : MonoBehaviour
 {
     public Text scoreText;
-
-    public float scoreIncraseRate = 1f; //skor artýþ hýzý
+   
     public float initialScore = 0f;
-    public Transform playerTransform;
-    private float playerStartPosition;
     public const string highScore_key = "HighScore";
+    [SerializeField] public float scoreIncraseRate = 1f; //skor artýþ hýzý
+    public Transform playerTransform;
+
     private bool isRunning = true;
-    private float highScore;
-    private float currentScore;
+    private float playerStartPosition;
+    private float _highScore;
+    private float _currentScore;
 
 
     public void Awake()
     {
-        highScore = PlayerPrefs.GetFloat(highScore_key);
-        Debug.Log("skor kaydedildi" + currentScore.ToString());
+        _highScore = PlayerPrefs.GetFloat(highScore_key);
+        Debug.Log("skor kaydedildi" + _currentScore.ToString());
+
+        // Baþlangýç pozisyonunu ayarla
+        playerStartPosition = playerTransform.position.z;
+        // Baþlangýç skorunu sýfýr yap
+        _currentScore = 0f;
+        // Skoru güncelle
+        UpdateScore();
 
     }
+
     public void SetRunningState(bool running)
     {
+
         isRunning = running;
+
         if (!isRunning)
         {
             SaveScore(); // Koþma durduðunda skoru kaydet
@@ -40,9 +51,9 @@ public class PlayerScore : MonoBehaviour
         if (isRunning)
         {
             //skoru her saniyede arttýrmak için
-            currentScore = playerTransform.position.z - playerStartPosition;
-            currentScore += currentScore * scoreIncraseRate * Time.deltaTime;
-            Debug.Log("score:" + currentScore.ToString());
+           _currentScore = playerTransform.position.z - playerStartPosition;
+            _currentScore += _currentScore * scoreIncraseRate * Time.deltaTime;
+            //Debug.Log("score:" + _currentScore.ToString());
 
             UpdateScore();
         }
@@ -53,12 +64,12 @@ public class PlayerScore : MonoBehaviour
     public void UpdateScore()
     {
 
-        if (currentScore >= highScore)
+        if (_currentScore >= _highScore)
         {
-            highScore = currentScore;
-            PlayerPrefs.SetFloat(highScore_key, highScore);
+            _highScore = _currentScore;
+            PlayerPrefs.SetFloat(highScore_key, _highScore);
             PlayerPrefs.Save();
-            Debug.Log("En yüksek skor güncellendi: " + highScore.ToString());
+           // Debug.Log("En yüksek skor güncellendi: " + _highScore.ToString());
 
             ChangeScoreColor();
 
@@ -68,27 +79,31 @@ public class PlayerScore : MonoBehaviour
 
 
         //skoru göster
-        scoreText.text = "Score: " + Mathf.Round(currentScore).ToString();
+        scoreText.text = "Score: " + Mathf.Round(_currentScore).ToString();
     }
+
 
     public void IncreaseScore(int amount)
     {
-        currentScore += amount;
+        _currentScore += amount;
         UpdateScore();
 
     }
+
 
     public void ResetScore()
     {
-        currentScore = 0;
+        _currentScore = 0;
         UpdateScore();
 
     }
+
     public void SaveScore()
     {
-        PlayerPrefs.SetFloat(highScore_key, currentScore);
+        PlayerPrefs.SetFloat(highScore_key, _currentScore);
         PlayerPrefs.Save();
     }
+
 
     private void ChangeScoreColor()
     {
